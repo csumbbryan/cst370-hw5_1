928f8f29c0ae6d2eec18546f8e982f5b03416eca
csumbbryan <bzanoli@csumb.edu>
2024-10-03 09:29:14 -0700

    Initial scan in with output of heap array before heap check or heapify

--- Main.java
+++ Main.java
@@ -13,13 +13,41 @@
  * Name: Write your name
  * Date: MM/DD/YYYY
  */
- 
-class Main 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main
 {
     public static void main(String[] args) {
 
         // Develop your program here.
         // The following is just a sample statement and and you need to replace it with your code
+        ArrayList<Integer> heapArray = new ArrayList<>();
+
+        //Read in Values
+        Scanner scanner = new Scanner(System.in);
+        int nodeCount = Integer.parseInt(scanner.nextLine().split(" ")[0]);
+        String arrLine = scanner.nextLine();
+        String[] strArr = arrLine.split(" ");
+        for (int i = 0; i < nodeCount; i++) {
+            heapArray.add(Integer.parseInt(strArr[i]));
+        }
+
+        //Test Output:
+        System.out.println(heapArray);
+
+        //Process the commands
+
+        //Heapify the array
+
+        //Insert nodes (then heapify)
+
+        //Delete nodes (then heapify)
+
+        //Delete the root node (then heapify)
+
+
         
         System.out.println("Hello world!");
     }
